6501705ad2d3364c881a255413bea0da38558ec0
Hical <h41kal.pr@gmail.com>
2024-08-14 22:22:44 +0700

    added JS make result tomorrow

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,15 @@
-console.log('linked Success');
 
 function formValidate() {
     let inputWeight = document.getElementById('input-weight').value;
     let inputHeight = document.getElementById('input-height').value;
-    console.log('Fill Weight Input: ${inputWeight}');
-    console.log('Fill Height Input: ${inputHeight}');
+    console.log(`Fill Weight Input: ${inputWeight}`);
+    console.log(`Fill Height Input: ${inputHeight}`);
     if (inputWeight == '' || inputHeight == '') {
-        alert('Body Weight Input Empty!');
+        alert('Body Weight And Or Height Input Empty!');
         } else {
             console.log(inputWeight * inputHeight);
-            let convertCmtoM = inputHeight * 100;
-            let finalHeight = Math.pow(convertCmtoM, 2);
+            let convertCmToM = inputHeight * 100;
+            let finalHeight = Math.pow(convertCmToM, 2);
             console.log(inputWeight/finalHeight); 
         }
         
