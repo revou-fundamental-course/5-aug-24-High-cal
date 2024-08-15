088eb89ff425a582901177ec6b937802870189df
Hical <h41kal.pr@gmail.com>
2024-08-15 23:30:19 +0700

    Finished

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,22 @@
 
-function formValidate() {
+function processForm() {
     let inputWeight = document.getElementById('input-weight').value;
     let inputHeight = document.getElementById('input-height').value;
     console.log(`Fill Weight Input: ${inputWeight}`);
     console.log(`Fill Height Input: ${inputHeight}`);
+    let convertCmToM;
+    let finalHeight;
+    let finalResult;
+    let bmiPoint;
     if (inputWeight == '' || inputHeight == '') {
         alert('Body Weight And Or Height Input Empty!');
         } else {
-            console.log(inputWeight * inputHeight);
             let convertCmToM = inputHeight * 100;
             let finalHeight = Math.pow(convertCmToM, 2);
-            console.log(inputWeight/finalHeight); 
+            let finalResult = inputHeight/finalHeight.toFixed(2);
+            console.log(finalResult);
+            let bmiPoint = finalResult;
+            document.getElementById("result").innerHTML = bmiPoint;
+
         }
-        
 }
